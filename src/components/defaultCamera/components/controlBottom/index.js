333072cf646d6ba5ed51010851dd860cc3056b17
Kit You <kmbagares@dnamicro.com>
2022-02-22 20:53:16 +0800

    fix circular menu:

--- src/components/defaultCamera/components/controlBottom/index.js
+++ src/components/defaultCamera/components/controlBottom/index.js
@@ -9,7 +9,6 @@ const ControlBottom = (props) => {
     isTriangleClicked,
     takePhoto,
   } = props
-  console.log("!!!!!!!!!!!!!!!!!!!!!", isTriangleClicked)
   return (
     <><div className={isTriangleClicked ? "control-bottom active" : "control-bottom"}>
       {
@@ -17,7 +16,7 @@ const ControlBottom = (props) => {
         <div className={isTriangleClicked ? "control-recording active" : "control-recording"}
           onClick={() => {
             console.log('RECORD');
-          } }
+          }}
         >
           <img src={RecordingIcon} alt="" />
         </div>
@@ -28,47 +27,42 @@ const ControlBottom = (props) => {
       >
         <img src={TakePhotoIcon} onClick={() => {
           takePhoto();
-        } } alt="" />
+        }} alt="" />
       </div>
       <div className="triangle-container"
         onClick={() => {
           setIsTriangleClicked(!isTriangleClicked);
-        } }
+        }}
       >
         <div className="control-triangle">
         </div>
         <div className="control-bold-line">
         </div>
       </div>
-    </div><div className={isTriangleClicked ? "control-menu active" : "control-menu"}>
-        <input className='input-config' type='checkbox' id='control-toggle' checked={isTriangleClicked} onClick={() => {
+    </div>
+      <div className={isTriangleClicked ? "control-menu active" : 'control-menu'}>
+        {/* <input className='input-config' type='checkbox' id='control-toggle' checked={isTriangleClicked} onClick={() => {
           setIsTriangleClicked(!isTriangleClicked);
-        } } />
-        <label for={"control-toggle"} id="show-control-menu">
-          <div className={'control-btn'}>
-            <div className={isTriangleClicked ? "triangle-container-down" : "triangle-container"}
-            >
-              <div className="control-triangle">
-              </div>
-              <div className="control-bold-line">
-              </div>
-            </div>
-          </div>
-          
-          <div className='control-btn circular'><i>1 </i></div>
-        
-          <div className='control-btn circular'><i>2 </i></div>
-          <div className='control-btn circular'><i>3 </i></div>
-          <div className='control-btn circular'><i>4 </i></div>
+        } } /> */}
+        {/* <label for={"control-toggle"} id="show-control-menu"> */}
+        <div className={'control-btn'}>
+        </div>
 
-          <div className='control-btn circular'><i>5 </i></div>
-          
-          <div className='control-btn circular'><i>6 </i></div>
-          <div className='control-btn circular'><i>7 </i></div>
-          <div className='control-btn circular'><i>8 </i></div>
-          {/* <div className='control-btn' ><i>11 </i></div> */}
-        </label>
-      </div></>
+        <div className='control-btn circular' ><i>1 </i></div>
+
+        <div className='control-btn circular' onClick={() => console.log('')}><i>2 </i></div>
+        <div className='control-btn circular'><i>3 </i></div>
+        <div className='control-btn circular'><i>4 </i></div>
+
+        <div className='control-btn circular'><i>5 </i></div>
+
+        <div className='control-btn circular'><i>6 </i></div>
+        <div className='control-btn circular'><i>7 </i></div>
+        <div className='control-btn circular'><i>8 </i></div>
+        {/* <div className='control-btn' ><i>11 </i></div> */}
+        {/* </label> */}
+      </div>
+    </>
   )
 }
 
